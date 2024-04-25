99a9c29c5955bfac72de0f82bc605d5f874bed8d
Abbygail Pollack <abbypollack00@gmail.com>
2024-04-25 11:21:38 -0400

    Implemented GlobalErrorHandler class for handling global exceptions and providing meaningful error messages and created ErrorResponse class for building error responses with appropriate HTTP status codes

--- src/main/java/com/mastermind/controllers/ErrorResponse.java
+++ src/main/java/com/mastermind/controllers/ErrorResponse.java
@@ -1,4 +1,28 @@
 package com.mastermind.controllers;
 
+import com.mastermind.domain.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ErrorResponse {
+
+    public static <T> ResponseEntity<Object> build(Result<T> result) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        switch (result.getType()) {
+            case INVALID:
+                status = HttpStatus.BAD_REQUEST;
+                break;
+            case NOT_FOUND:
+                status = HttpStatus.NOT_FOUND;
+                break;
+            case SUCCESS:
+                status = HttpStatus.OK;
+                break;
+            default:
+                status = HttpStatus.INTERNAL_SERVER_ERROR;
+                break;
+        }
+        return new ResponseEntity<>(result.getMessages(), status);
+    }
+
 }
--- src/main/java/com/mastermind/controllers/GlobalErrorHandler.java
+++ src/main/java/com/mastermind/controllers/GlobalErrorHandler.java
@@ -1,4 +1,44 @@
 package com.mastermind.controllers;
 
-public class GlobalErrorHandler {
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        String message = "Data integrity issue encountered. Please ensure that all provided data fields meet uniqueness and validity constraints.";
+        return new ResponseEntity<>(message, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex) {
+        String message = "The requested resource was not found. Please check the identifier provided and try again.";
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String message = String.format("Parameter '%s' is of invalid type. Expected type: '%s'.", ex.getName(), ex.getRequiredType().getSimpleName());
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAllUncaughtException(Exception ex) {
+        String message = "An unexpected error occurred. Please contact support if the problem persists.";
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<String> handleIllegalStateException(IllegalStateException ex) {
+        String message = ex.getMessage();
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
 }
